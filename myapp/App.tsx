37bc1105a15fddca67c4a9199794c4000ce48fa5
ouioui99 <qp5minute@gmail.com>
2025-03-11 08:51:07 +0900

    supabaseにてsignupおよびlogin実行（チュートリアル）

--- myapp/App.tsx
+++ myapp/App.tsx
@@ -1,17 +1,30 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./screens/HomeScreen";
-import UserScreen from "./screens/UserScreen";
-
-const Stack = createNativeStackNavigator();
+import { useState, useEffect } from "react";
+import { supabase } from "./lib/supabase";
+import Auth from "./screens/Auth";
+import Account from "./screens/Account";
+import { View } from "react-native";
+import { Session } from "@supabase/supabase-js";
 
 export default function App() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="User" component={UserScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View>
+      {session && session.user ? (
+        <Account key={session.user.id} session={session} />
+      ) : (
+        <Auth />
+      )}
+    </View>
   );
 }
